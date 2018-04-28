9834f5d57bfdc2d88600ec3ae7870e1796f480c9
DavidBuchanan314 <d@vidbuchanan.co.uk>
2018-04-28 12:58:49 +0100

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring
+
+JNICALL
+Java_io_github_davidbuchanan314_nxloader_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
